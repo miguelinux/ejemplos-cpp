77c2c67d72ef81ccd522f3ee6310a32a51cb29d9
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-11-24 08:17:32 -0600

    add 01 example
    Signed-off-by: Miguel Bernal Marin <mbernal@itszapopan.edu.mx>

---
+++ threads/01-example.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <thread>
+
+
+void func(int i, double d, const std::string& s)
+{
+    std::cout << i << ", " << d << ", " << s << std::endl;
+}
+ 
+int main()
+{
+   std::thread t(func, 1, 12.50, "sample");
+   t.join();
+ 
+   return 0;
+}
+
