423b210ec938153ab70c726e8c2871b3aeed8a92
Miguel Bernal Marin <mbernal@itszapopan.edu.mx>
2015-11-24 08:34:37 -0600

    add exaple 3, try catch
    is not finished yet, some warnings appear in the console
    
    Signed-off-by: Miguel Bernal Marin <mbernal@itszapopan.edu.mx>

---
+++ threads/03-try-catch.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <iostream>
+
+#include <vector>
+
+#include <exception>
+
+#include <thread>
+#include <mutex>
+
+
+std::mutex                       g_mutex;
+std::vector<std::exception_ptr>  g_exceptions;
+
+void throw_function()
+{
+   throw std::exception("something wrong happened");
+}
+
+void func()
+{
+   try
+   {
+      throw_function();
+   }
+   catch(...)
+   {
+      std::lock_guard<std::mutex> lock(g_mutex);
+      g_exceptions.push_back(std::current_exception());
+   }
+}
+
+int main()
+{
+   g_exceptions.clear();
+
+   std::thread t(func);
+   t.join();
+
+   for(auto& e : g_exceptions)
+   {
+      try 
+      {
+         if(e != nullptr)
+         {
+            std::rethrow_exception(e);
+         }
+      }
+      catch(const std::exception& e)
+      {
+         std::cout << e.what() << std::endl;
+      }
+   }
+
+   return 0;
+}
+
+
+
